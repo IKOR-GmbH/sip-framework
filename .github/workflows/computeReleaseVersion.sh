1cd058455044c1136df4f7ffb5d82fe7f13d8bad
Thomas Stieglmaier <thomas.stieglmaier@ikor.de>
2021-11-16 10:01:25 +0100

    compute new release version automatically from changelogs

---
+++ .github/workflows/computeReleaseVersion.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+CUR_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+
+MAJOR=$(echo $CUR_VERSION | cut -d '.' -f 1)
+FEATURE=$(echo $CUR_VERSION | cut -d '.' -f 2)
+PATCH=$(echo $CUR_VERSION | cut -d '.' -f 3 | cut -d '-' -f 1)
+
+if test -n "$(find changelogs/major -name '*.json' -print -quit)"; then
+  MAJOR=$(($PATCH+1))
+elif test -n "$(find changelogs/feature -name '*.json' -print -quit)"; then
+  FEATURE=$(($PATCH+1))
+else
+  PATCH=$(($PATCH+1))
+fi
+
+echo "$MAJOR.$FEATURE.$PATCH"
+exit 0
