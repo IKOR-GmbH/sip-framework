cc33f00ae32c69c6cf2bb2c5cc6607e520835f63
Igor Smirnov <igor.smirnov@ikor.de>
2021-12-17 11:30:28 +0100

    Format code

--- sip-core/src/test/java/de/ikor/sip/foundation/core/trace/CustomTracerTypeTest.java
+++ sip-core/src/test/java/de/ikor/sip/foundation/core/trace/CustomTracerTypeTest.java
@@ -1,5 +1,8 @@
 package de.ikor.sip.foundation.core.trace;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
 import org.apache.camel.CamelContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,9 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
 
 @ExtendWith(SpringExtension.class)
 @EnableConfigurationProperties(value = SIPTraceConfig.class)
