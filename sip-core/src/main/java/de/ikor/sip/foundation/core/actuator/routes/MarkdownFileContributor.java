c575eee5b5147c54028171263b611778ce3dcf1f
smirglagit <57665309+smirglagit@users.noreply.github.com>
2021-12-23 09:19:25 +0100

    review changes

--- sip-core/src/main/java/de/ikor/sip/foundation/core/actuator/routes/MarkdownFileContributor.java
+++ sip-core/src/main/java/de/ikor/sip/foundation/core/actuator/routes/MarkdownFileContributor.java
@@ -7,8 +7,6 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.actuate.info.Info;
 import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.stereotype.Component;
