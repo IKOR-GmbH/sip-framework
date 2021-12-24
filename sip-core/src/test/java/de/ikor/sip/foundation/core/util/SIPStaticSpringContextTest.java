3b4a877e471f92ca0cb83b068deb4059396de565
Nemikor <nemanja.nedeljkovic@ikor.de>
2021-12-24 12:26:04 +0100

    Refactor util unit tests

--- sip-core/src/test/java/de/ikor/sip/foundation/core/util/SIPStaticSpringContextTest.java
+++ sip-core/src/test/java/de/ikor/sip/foundation/core/util/SIPStaticSpringContextTest.java
@@ -5,59 +5,65 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
-import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 class SIPStaticSpringContextTest {
 
-  ApplicationContext applicationContext;
-  SIPStaticSpringContext sipStaticSpringContext;
+  private ApplicationContext applicationContext;
+  private SIPStaticSpringContext subject;
 
   @BeforeEach
   void setUp() {
     applicationContext = mock(ApplicationContext.class);
-    sipStaticSpringContext = new SIPStaticSpringContext();
+    subject = new SIPStaticSpringContext();
   }
 
   @Test
   @DisplayName(
       "Given any bean from context, when setting field context, then this and context bean shall be equal")
-  void getBean() {
+  void When_getBeanFromStaticContext_Expect_BeanReturned() {
     // arrange
     Object bean = new Object();
     when(applicationContext.getBean(any(Class.class))).thenReturn(bean);
 
     // act
-    ReflectionTestUtils.setField(sipStaticSpringContext, "context", applicationContext);
+    subject.setApplicationContext( applicationContext);
 
     // assert
-    assertThat(bean).isEqualTo(SIPStaticSpringContext.getBean((Object.class)));
+    assertThat(SIPStaticSpringContext.getBean((Object.class))).isEqualTo(bean);
   }
 
   @Test
-  @DisplayName(
-      "Given any bean from context, when setting field context, then this and context bean shall be equal")
-  void getBean_ExceptionThrown() {
+  void When_getBeanWithMissingBean_Expect_noBeanReturned() {
     // arrange
     when(applicationContext.getBean(any(Class.class))).thenThrow(new RuntimeException());
+    Logger logger =
+            (Logger)
+                    LoggerFactory.getLogger(
+                            "de.ikor.sip.foundation.core.util.SIPStaticSpringContext");
+    ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
+    listAppender.start();
+    logger.addAppender(listAppender);
 
-    // act
-    ReflectionTestUtils.setField(sipStaticSpringContext, "context", applicationContext);
+    List<ILoggingEvent> logsList = listAppender.list;
+
+    //act
+    subject.setApplicationContext( applicationContext);
 
     // assert
     assertThat(SIPStaticSpringContext.getBean(Object.class)).isNull();
-  }
-
-  @Test
-  void setApplicationContext() {
-    // arrange
-    sipStaticSpringContext.setApplicationContext(applicationContext);
-
-    // assert
-    assertThat(applicationContext)
-        .isEqualTo(ReflectionTestUtils.getField(sipStaticSpringContext, "context"));
+    assertThat(logsList.get(0).getMessage()).isEqualTo("sip.core.util.missingbean_{}");
+    assertThat(logsList.get(0).getLevel()).isEqualTo(Level.DEBUG);
   }
 }
