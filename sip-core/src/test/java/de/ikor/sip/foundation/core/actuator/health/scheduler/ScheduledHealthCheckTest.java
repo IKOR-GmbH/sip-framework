cf59eb3b2a99b44f3e43a1ebcaa5c232c3dcefaa
nikola.glidzic <nikola.glidzic@ikor.de>
2021-12-27 13:06:42 +0100

    Scheduler test refactoring

--- sip-core/src/test/java/de/ikor/sip/foundation/core/actuator/health/scheduler/ScheduledHealthCheckTest.java
+++ sip-core/src/test/java/de/ikor/sip/foundation/core/actuator/health/scheduler/ScheduledHealthCheckTest.java
@@ -16,9 +16,11 @@ import org.springframework.boot.actuate.health.Health;
 class ScheduledHealthCheckTest {
 
   private static final String ENDPOINT = "endpoint";
+  private static final Health HEALTH_STATUS_UP = Health.up().build();
 
   private CamelEndpointHealthMonitor camelEndpointHealthMonitor;
   private ScheduledHealthCheck scheduledHealthCheckSubject;
+
 
   @Test
   void WHEN_scheduledExecution_EXPECT_HealthEndpointsAreCalculated() {
@@ -27,7 +29,7 @@ class ScheduledHealthCheckTest {
     scheduledHealthCheckSubject = new ScheduledHealthCheck(camelEndpointHealthMonitor);
 
     Map<String, EndpointHealthIndicator> healthIndicators = new HashMap<>();
-    Function<Endpoint, Health> healthFunction = endpoint -> Health.up().build();
+    Function<Endpoint, Health> healthFunction = endpoint -> HEALTH_STATUS_UP;
     EndpointHealthIndicator endpointHealthIndicator =
         new EndpointHealthIndicator(mock(Endpoint.class), healthFunction);
     healthIndicators.put(ENDPOINT, endpointHealthIndicator);
@@ -39,6 +41,6 @@ class ScheduledHealthCheckTest {
     Health healthResult = healthIndicators.get(ENDPOINT).getHealth(false);
 
     // assert
-    assertThat(healthResult).isEqualTo(Health.up().build());
+    assertThat(healthResult).isEqualTo(HEALTH_STATUS_UP);
   }
 }
