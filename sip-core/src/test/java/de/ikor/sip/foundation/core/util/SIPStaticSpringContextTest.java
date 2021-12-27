c0a08c51b36fa5443de81f4badbb7e676226660e
Nemikor <nemanja.nedeljkovic@ikor.de>
2021-12-27 16:42:34 +0100

    Revert some changes

--- sip-core/src/test/java/de/ikor/sip/foundation/core/util/SIPStaticSpringContextTest.java
+++ sip-core/src/test/java/de/ikor/sip/foundation/core/util/SIPStaticSpringContextTest.java
@@ -1,21 +1,15 @@
 package de.ikor.sip.foundation.core.util;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-import ch.qos.logback.classic.Level;
-import ch.qos.logback.classic.Logger;
-import ch.qos.logback.classic.spi.ILoggingEvent;
-import ch.qos.logback.core.read.ListAppender;
-import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class SIPStaticSpringContextTest {
 
@@ -45,19 +39,10 @@ class SIPStaticSpringContextTest {
   void When_getNonExistingBean_Expect_nullBean() {
     // arrange
     when(applicationContext.getBean(any(Class.class))).thenThrow(new RuntimeException());
-    Logger logger =
-        (Logger) LoggerFactory.getLogger("de.ikor.sip.foundation.core.util.SIPStaticSpringContext");
-    ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
-    listAppender.start();
-    logger.addAppender(listAppender);
-
-    List<ILoggingEvent> logsList = listAppender.list;
     subject.setApplicationContext(applicationContext);
 
     // assert
     assertThat(SIPStaticSpringContext.getBean(Object.class)).isNull();
-    assertThat(logsList.get(0).getMessage()).isEqualTo("sip.core.util.missingbean_{}");
-    assertThat(logsList.get(0).getLevel()).isEqualTo(Level.DEBUG);
   }
 
   @Test
