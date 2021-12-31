835c1f9a2769311f01506421996d625f40a03b79
Nemikor <nemanja.nedeljkovic@ikor.de>
2021-12-31 14:39:48 +0100

    Formatting

--- sip-core/src/main/java/de/ikor/sip/foundation/core/actuator/health/scheduler/HealthCheckEnabledCondition.java
+++ sip-core/src/main/java/de/ikor/sip/foundation/core/actuator/health/scheduler/HealthCheckEnabledCondition.java
@@ -1,15 +1,13 @@
 package de.ikor.sip.foundation.core.actuator.health.scheduler;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE})
 @ConditionalOnExpression(
-        "${sip.core.metrics.scheduled-health-check.enabled:true} and '${management.endpoints.web.exposure.include}'.contains('health')")
-public @interface HealthCheckEnabledCondition {
-}
+    "${sip.core.metrics.scheduled-health-check.enabled:true} and '${management.endpoints.web.exposure.include}'.contains('health')")
+public @interface HealthCheckEnabledCondition {}
