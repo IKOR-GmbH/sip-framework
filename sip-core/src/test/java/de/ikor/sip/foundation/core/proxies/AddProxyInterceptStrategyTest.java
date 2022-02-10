d9e2f0b18d0f03d9808dd98f9b3980f569ab8aa6
Leto Bukarica <leto.bukarica@gmail.com>
2022-02-10 19:39:16 +0100

    SIP-946|added release notes & updated tests

--- sip-core/src/test/java/de/ikor/sip/foundation/core/proxies/AddProxyInterceptStrategyTest.java
+++ sip-core/src/test/java/de/ikor/sip/foundation/core/proxies/AddProxyInterceptStrategyTest.java
@@ -1,18 +1,23 @@
 package de.ikor.sip.foundation.core.proxies;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import de.ikor.sip.foundation.core.proxies.extension.ProxyExtension;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import org.apache.camel.Endpoint;
 import org.apache.camel.NamedNode;
+import org.apache.camel.Processor;
+import org.apache.camel.processor.SendProcessor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
 
 @ExtendWith(MockitoExtension.class)
 class AddProxyInterceptStrategyTest {
@@ -38,5 +43,38 @@ class AddProxyInterceptStrategyTest {
     // assert
     Optional<ProcessorProxy> proxy = proxyRegistry.getProxy(PROCESSOR_ID);
     assertThat(proxy).isPresent();
+    assertThat(proxyRegistry.getProxies()).hasSize(1);
+  }
+
+  @Test
+  void WHEN_wrapEndpointProcessor_WITH_validParams_THEN_endpointProcessorTrue() throws Exception {
+    // arrange
+    SendProcessor outgoingProcessor = mock(SendProcessor.class);
+    Endpoint outgoingEndpoint = mock(Endpoint.class);
+    when(outgoingProcessor.getEndpoint()).thenReturn(outgoingEndpoint);
+    when(outgoingEndpoint.getEndpointUri()).thenReturn("file://test.txt");
+    // act
+    addProxyInterceptStrategy.wrapProcessorInInterceptors(null, definition, outgoingProcessor, outgoingProcessor);
+
+    // assert
+    Optional<ProcessorProxy> proxy = proxyRegistry.getProxy(PROCESSOR_ID);
+    assertThat(proxy).isPresent();
+    assertThat(proxy.get().isEndpointProcessor()).isTrue();
+  }
+
+  @Test
+  void WHEN_wrapEndpointProcessor_WITH_ignoredEndpoint_THEN_endpointProcessorFalse() throws Exception {
+    // arrange
+    SendProcessor outgoingProcessor = mock(SendProcessor.class);
+    Endpoint outgoingEndpoint = mock(Endpoint.class);
+    when(outgoingProcessor.getEndpoint()).thenReturn(outgoingEndpoint);
+    when(outgoingEndpoint.getEndpointUri()).thenReturn("sipmc:middleComponent");
+    // act
+    addProxyInterceptStrategy.wrapProcessorInInterceptors(null, definition, outgoingProcessor, outgoingProcessor);
+
+    // assert
+    Optional<ProcessorProxy> proxy = proxyRegistry.getProxy(PROCESSOR_ID);
+    assertThat(proxy).isPresent();
+    assertThat(proxy.get().isEndpointProcessor()).isFalse();
   }
 }
