2122ebfeb484e4018b8587366e1bd98beff85889
nikola.glidzic <nikola.glidzic@ikor.de>
2022-02-18 15:50:15 +0100

    Refactoring Unit test which tests the files reading method from the classpath.

--- sip-core/src/test/java/de/ikor/sip/foundation/core/actuator/routes/MarkdownFilesContributorTest.java
+++ sip-core/src/test/java/de/ikor/sip/foundation/core/actuator/routes/MarkdownFilesContributorTest.java
@@ -2,6 +2,12 @@ package de.ikor.sip.foundation.core.actuator.routes;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.LinkedHashMap;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,6 +19,12 @@ class MarkdownFilesContributorTest {
   private static final String BUILD_KEY = "build";
   private static final String FILES_KEY = "files";
 
+  private static final String FILES_TARGET_TESTING_DIRECTORY = "target/test-classes/documents/";
+  private static final String README_MD = "readme.md";
+  private static final String CHANGELOG_MD = "changelog.md";
+  private static final String FILENAME_TXT = "test.txt";
+  private static final String FILE_CONTENT = "Test data";
+
   private MarkdownFilesContributor subject;
   private Info.Builder builder;
 
@@ -23,9 +35,23 @@ class MarkdownFilesContributorTest {
   }
 
   @Test
-  void Given_markdownFilesInDocumentsFolder_When_contribute_Then_returnOnlyMarkdownFiles() {
+  void Given_markdownAndTextFilesInDocumentsFolder_When_contribute_Then_returnOnlyMarkdownFiles() throws Exception {
+    // arrange
+    Path directory = Paths.get(FILES_TARGET_TESTING_DIRECTORY);
+    Files.createDirectories(directory);
+
+    File file1 = createFile(FILES_TARGET_TESTING_DIRECTORY, README_MD, FILE_CONTENT);
+    File file2 = createFile(FILES_TARGET_TESTING_DIRECTORY, CHANGELOG_MD, FILE_CONTENT);
+    File file3 = createFile(FILES_TARGET_TESTING_DIRECTORY, FILENAME_TXT, FILE_CONTENT);
+
     // act
     subject.contribute(builder);
+
+    // Keep environment clean
+    file1.delete();
+    file2.delete();
+    file3.delete();
+    Files.deleteIfExists(directory);
 
     // assert
     LinkedHashMap<String, Object> buildInfoResult =
@@ -33,6 +59,14 @@ class MarkdownFilesContributorTest {
     List<MarkdownObject> resultFiles = (List<MarkdownObject>) buildInfoResult.get(FILES_KEY);
 
     assertThat(buildInfoResult).hasSize(1);
-    assertThat(resultFiles).hasSize(3);
+    assertThat(resultFiles).hasSize(2);
+  }
+
+  private File createFile(String directory, String fileName, String fileContent) throws IOException {
+    File file = new File(directory + fileName);
+    FileWriter writer = new FileWriter(file);
+    writer.write(fileContent);
+    writer.close();
+    return file;
   }
 }
--- sip-core/src/test/resources/documents/adapter-description.md
+++
@@ -1,3 +0,0 @@
-# SIP Adapter Description
-
-This file should contain details about what your adapter does, which services does it connect, what is the workflow etc.
