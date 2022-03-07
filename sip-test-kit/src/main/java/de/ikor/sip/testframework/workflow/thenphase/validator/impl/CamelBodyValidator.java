ea70c00b03697ddbf387cbc41747ef19491fce50
vladimir.tucakovic <vladimir.tucakovic@ikor.de>
2022-03-07 12:06:37 +0100

    - fix SIP-1031

--- sip-test-kit/src/main/java/de/ikor/sip/testframework/workflow/thenphase/validator/impl/CamelBodyValidator.java
+++ sip-test-kit/src/main/java/de/ikor/sip/testframework/workflow/thenphase/validator/impl/CamelBodyValidator.java
@@ -1,11 +1,13 @@
 package de.ikor.sip.testframework.workflow.thenphase.validator.impl;
 
+import static org.apache.camel.support.MessageHelper.extractBodyAsString;
+import static org.apache.camel.support.MessageHelper.resetStreamCache;
+
 import de.ikor.sip.testframework.util.RegexUtil;
 import de.ikor.sip.testframework.workflow.thenphase.result.ValidationResult;
 import de.ikor.sip.testframework.workflow.thenphase.validator.ExchangeValidator;
 import lombok.AllArgsConstructor;
 import org.apache.camel.Exchange;
-import org.apache.camel.support.MessageHelper;
 import org.springframework.stereotype.Component;
 
 /** Validator for body of a request in Camel */
@@ -16,23 +18,23 @@ public class CamelBodyValidator implements ExchangeValidator {
   /**
    * Invokes compare body content
    *
-   * @param executionResult Result of test execution
+   * @param actualResult Result of test execution
    * @param expectedResponse Expected result of test execution
    * @return {@link ValidationResult}
    */
   @Override
-  public ValidationResult execute(Exchange executionResult, Exchange expectedResponse) {
+  public ValidationResult execute(Exchange actualResult, Exchange expectedResponse) {
+    resetStreamCache(actualResult.getMessage());
     boolean result =
         RegexUtil.compare(
-            MessageHelper.extractBodyAsString(expectedResponse.getMessage()),
-            MessageHelper.extractBodyAsString(executionResult.getMessage()));
+            extractBodyAsString(expectedResponse.getMessage()),
+            extractBodyAsString(actualResult.getMessage()));
     return new ValidationResult(
         result, result ? "Body validation successful" : "Body validation unsuccessful");
   }
 
   @Override
   public boolean isApplicable(Exchange executionResult, Exchange expectedResponse) {
-    return expectedResponse != null
-        && MessageHelper.extractBodyAsString(expectedResponse.getMessage()) != null;
+    return expectedResponse != null && extractBodyAsString(expectedResponse.getMessage()) != null;
   }
 }
