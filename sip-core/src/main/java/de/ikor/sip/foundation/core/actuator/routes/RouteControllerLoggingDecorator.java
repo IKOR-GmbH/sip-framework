44a6de631000bc937412f79f08e00d7e44cf8b68
vladimir.tucakovic <vladimir.tucakovic@ikor.de>
2022-03-09 13:26:40 +0100

    Javadocs update

--- sip-core/src/main/java/de/ikor/sip/foundation/core/actuator/routes/RouteControllerLoggingDecorator.java
+++ sip-core/src/main/java/de/ikor/sip/foundation/core/actuator/routes/RouteControllerLoggingDecorator.java
@@ -8,22 +8,46 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 @Slf4j
+/**
+ * Class used by {@link RouteOperation} instead of calling {@link org.apache.camel.spi.RouteController}, in
+ * order to add missing logs.
+ */
 public class RouteControllerLoggingDecorator {
   private final CamelContext ctx;
 
+  /**
+   * Starting route and logging the action
+   * @param routeId - Id of route to be started
+   * @throws Exception
+   */
   public void startRoute(String routeId) throws Exception {
     ctx.getRouteController().startRoute(routeId);
     log.info("Started {} - ({})", routeId, getRouteEndpointUri(routeId));
   }
 
+  /**
+   * Stopping route
+   * @param routeId - Id of route to be stopped
+   * @throws Exception
+   */
   public void stopRoute(String routeId) throws Exception {
     ctx.getRouteController().stopRoute(routeId);
   }
 
+  /**
+   * Suspending route
+   * @param routeId - Id of route to be suspended
+   * @throws Exception
+   */
   public void suspendRoute(String routeId) throws Exception {
     ctx.getRouteController().suspendRoute(routeId);
   }
 
+  /**
+   * Resuming route and logging the action
+   * @param routeId - Id of route to be resumed
+   * @throws Exception
+   */
   public void resumeRoute(String routeId) throws Exception {
     ctx.getRouteController().resumeRoute(routeId);
     log.info("Resumed {} - ({})", routeId, getRouteEndpointUri(routeId));
