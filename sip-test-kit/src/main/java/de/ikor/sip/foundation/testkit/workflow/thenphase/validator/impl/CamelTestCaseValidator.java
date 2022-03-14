66c9648c31aa1167d36b25017fbd15e8f2ccddbd
Leto Bukarica <leto.bukarica@gmail.com>
2022-03-14 11:18:24 +0100

    SIP-1052|testkit package name change

---
+++ sip-test-kit/src/main/java/de/ikor/sip/foundation/testkit/workflow/thenphase/validator/impl/CamelTestCaseValidator.java
@@ -0,0 +1,114 @@
+package de.ikor.sip.foundation.testkit.workflow.thenphase.validator.impl;
+
+import de.ikor.sip.foundation.testkit.workflow.TestExecutionStatus;
+import de.ikor.sip.foundation.testkit.workflow.reporting.model.EndpointValidationOutcome;
+import de.ikor.sip.foundation.testkit.workflow.reporting.model.MockReport;
+import de.ikor.sip.foundation.testkit.workflow.reporting.model.SIPAdapterExecutionReport;
+import de.ikor.sip.foundation.testkit.workflow.thenphase.result.ValidationResult;
+import de.ikor.sip.foundation.testkit.workflow.thenphase.result.ValidationType;
+import de.ikor.sip.foundation.testkit.workflow.thenphase.validator.ExchangeValidator;
+import de.ikor.sip.foundation.testkit.workflow.thenphase.validator.TestCaseValidator;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.camel.Exchange;
+import org.springframework.stereotype.Component;
+
+/** Batch test validator for Camel */
+@Slf4j
+@Component
+@AllArgsConstructor
+public class CamelTestCaseValidator implements TestCaseValidator {
+  private final List<ExchangeValidator> exchangeValidators;
+
+  @Override
+  public ValidationType getValidationType() {
+    return ValidationType.FULL;
+  }
+
+  /** Validates actual of test execution and forwards it to report service */
+  @Override
+  public void validate(TestExecutionStatus testExecutionStatus) {
+    SIPAdapterExecutionReport adapterReport = testExecutionStatus.getAdapterReport();
+    Map<String, MockReport> mockReports = testExecutionStatus.getMockReports();
+
+    this.validateAdapterResponse(adapterReport);
+    this.validateMockReports(mockReports);
+
+    boolean isAdapterResultExpected =
+        evaluateValidationResults(adapterReport.getValidationResults());
+    boolean areAllMocksExpected = mockReports.values().stream().noneMatch(this::isNotSuccess);
+
+    testExecutionStatus.setSuccessfulExecution(isAdapterResultExpected && areAllMocksExpected);
+  }
+
+  private void validateAdapterResponse(SIPAdapterExecutionReport adapterExecutionReport) {
+    Exchange actual = adapterExecutionReport.getActualResponse();
+    Exchange expected = adapterExecutionReport.getExpectedResponse();
+    List<ValidationResult> adapterValidationResults = runValidators(actual, expected);
+    adapterExecutionReport
+        .setValidationResults(adapterValidationResults)
+        .setValidatedHeaders(extractValidatedHeaders(actual, expected))
+        .setAdapterExceptionMessage(actual.getException());
+  }
+
+  private void validateMockReports(Map<String, MockReport> mockReportMap) {
+    mockReportMap.values().stream()
+        .filter(mockReport -> mockReport.getExpected() != null)
+        .forEach(this::fillMockReport);
+  }
+
+  private void fillMockReport(MockReport mockReport) {
+    List<ValidationResult> endpointValidationResultList =
+        runValidators(mockReport.getActual(), mockReport.getExpected());
+    mockReport.setValidated(
+        evaluateValidationResults(endpointValidationResultList)
+            ? EndpointValidationOutcome.SUCCESSFUL
+            : EndpointValidationOutcome.UNSUCCESSFUL);
+    mockReport.setValidatedHeaders(
+        extractValidatedHeaders(mockReport.getActual(), mockReport.getExpected()));
+  }
+
+  private boolean isNotSuccess(MockReport mockReport) {
+    return mockReport.getValidated().equals(EndpointValidationOutcome.UNSUCCESSFUL);
+  }
+
+  private HashMap<String, Object> extractValidatedHeaders(
+      Exchange executionResult, Exchange expectedResponse) {
+    HashMap<String, Object> validatedHeaders = new HashMap<>();
+    if (executionResult == null || expectedResponse == null) {
+      return validatedHeaders;
+    }
+    expectedResponse
+        .getMessage()
+        .getHeaders()
+        .keySet()
+        .forEach(
+            key -> {
+              if (executionResult.getMessage().getHeader(key) != null) {
+                validatedHeaders.put(
+                    key, executionResult.getMessage().getHeader(key, String.class));
+              }
+            });
+    return validatedHeaders;
+  }
+
+  private boolean evaluateValidationResults(List<ValidationResult> validationResultList) {
+    return validationResultList.stream().allMatch(ValidationResult::isSuccess);
+  }
+
+  private List<ValidationResult> runValidators(
+      Exchange executionResult, Exchange expectedResponse) {
+    List<ValidationResult> validationResults = new ArrayList<>();
+    for (ExchangeValidator validator : this.exchangeValidators) {
+      if (validator.isApplicable(executionResult, expectedResponse)) {
+        validationResults.add(validator.execute(executionResult, expectedResponse));
+      }
+    }
+
+    return validationResults;
+  }
+}
---
+++ sip-test-kit/src/test/java/de/ikor/sip/foundation/testkit/workflow/givenphase/ReportActivityProxyExtensionTest.java
@@ -0,0 +1,125 @@
+package de.ikor.sip.foundation.testkit.workflow.givenphase;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
+import de.ikor.sip.foundation.core.proxies.ProcessorProxy;
+import de.ikor.sip.foundation.testkit.workflow.TestCase;
+import de.ikor.sip.foundation.testkit.workflow.TestExecutionStatus;
+import de.ikor.sip.foundation.testkit.workflow.whenphase.executor.Executor;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ReportActivityProxyExtensionTest {
+  private final String TEST_NAME = "test name";
+
+  TestCase testCase;
+  ReportActivityProxyExtension subject;
+  Exchange original;
+  Exchange current;
+  Message message;
+  ProcessorProxy proxy;
+  List<TestCase> testCases;
+
+  @BeforeEach
+  void setUp() {
+    testCase = mock(TestCase.class);
+    subject = new ReportActivityProxyExtension();
+    original = mock(Exchange.class, RETURNS_DEEP_STUBS);
+    current = mock(Exchange.class, RETURNS_DEEP_STUBS);
+    message = mock(Message.class);
+    proxy = mock(ProcessorProxy.class);
+    when(testCase.getTestName()).thenReturn(TEST_NAME);
+    testCases = new ArrayList<>(Collections.singletonList(testCase));
+    subject.setTestCases(testCases);
+  }
+
+  @Test
+  void When_run_Expect_GeneratedReport() {
+    // arrange
+    TestExecutionStatus testExecutionStatus = new TestExecutionStatus();
+    when(original.getMessage()).thenReturn(message);
+    when(message.getHeader(Executor.TEST_NAME_HEADER, String.class)).thenReturn(TEST_NAME);
+    String ORIGINAL_BODY = "originalbody";
+    when(message.getBody()).thenReturn(ORIGINAL_BODY);
+    String PROXY_ID = "proxy id";
+    when(proxy.getId()).thenReturn(PROXY_ID);
+    when(testCase.getTestExecutionStatus()).thenReturn(testExecutionStatus);
+
+    // act
+    subject.run(proxy, original, current);
+
+    // assert
+    assertThat(testExecutionStatus.getMockReports().get(PROXY_ID)).isNotNull();
+  }
+
+  @Test
+  void When_run_With_NoTestCases_Then_exception() {
+    // arrange
+    subject.setTestCases(new ArrayList<>());
+    when(original.getMessage().getHeader(Executor.TEST_NAME_HEADER, String.class))
+        .thenReturn(TEST_NAME);
+
+    // act + assert
+    assertThatThrownBy(() -> subject.run(proxy, original, current))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("Test case with name " + TEST_NAME + " could not be found!");
+  }
+
+  @Test
+  void When_isApplicable_With_AllConditionsMatch_Then_true() {
+    when(proxy.isEndpointProcessor()).thenReturn(true);
+    when(original.getMessage().getHeader(Executor.TEST_NAME_HEADER, String.class))
+        .thenReturn(TEST_NAME);
+    when(original.getIn().getHeader(ProcessorProxy.TEST_MODE_HEADER, String.class))
+        .thenReturn("true");
+    assertTrue(subject.isApplicable(proxy, original, current));
+  }
+
+  @Test
+  void When_isApplicable_With_NonEndpointProcessor_Then_false() {
+    when(proxy.isEndpointProcessor()).thenReturn(false);
+    when(original.getMessage().getHeader(Executor.TEST_NAME_HEADER, String.class))
+        .thenReturn(TEST_NAME);
+    when(original.getIn().getHeader(ProcessorProxy.TEST_MODE_HEADER, String.class))
+        .thenReturn("true");
+    assertFalse(subject.isApplicable(proxy, original, current));
+  }
+
+  @Test
+  void When_isApplicable_With_NoTestCases_Then_false() {
+    subject.setTestCases(null);
+    when(original.getMessage().getHeader(Executor.TEST_NAME_HEADER, String.class))
+        .thenReturn(TEST_NAME);
+    when(proxy.isEndpointProcessor()).thenReturn(true);
+    when(original.getIn().getHeader(ProcessorProxy.TEST_MODE_HEADER, String.class))
+        .thenReturn("true");
+    assertFalse(subject.isApplicable(proxy, original, current));
+  }
+
+  @Test
+  void When_isApplicable_With_TestModeFalse_Then_false() {
+    when(proxy.isEndpointProcessor()).thenReturn(true);
+    when(original.getMessage().getHeader(Executor.TEST_NAME_HEADER, String.class))
+        .thenReturn(TEST_NAME);
+    when(original.getIn().getHeader(ProcessorProxy.TEST_MODE_HEADER, String.class))
+        .thenReturn("false");
+    assertFalse(subject.isApplicable(proxy, original, current));
+  }
+
+  @Test
+  void When_isApplicable_With_NoTestName_Then_false() {
+    when(proxy.isEndpointProcessor()).thenReturn(true);
+    when(original.getIn().getHeader(ProcessorProxy.TEST_MODE_HEADER, String.class))
+        .thenReturn("true");
+    assertFalse(subject.isApplicable(proxy, original, current));
+  }
+}
