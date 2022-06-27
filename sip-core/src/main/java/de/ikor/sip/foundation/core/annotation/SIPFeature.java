9dff27de6f2a71e5105ed879776c495af4af2527
Nemikor <nemanja.nedeljkovic@ikor.de>
2022-06-27 14:40:59 +0200

    Add javadoc

--- sip-core/src/main/java/de/ikor/sip/foundation/core/annotation/SIPFeature.java
+++ sip-core/src/main/java/de/ikor/sip/foundation/core/annotation/SIPFeature.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation which marks and names all framework features
+ */
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface SIPFeature {
