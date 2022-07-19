890621d471c620ec9f12eb1c66a44aedcbcdc633
Nemikor <nemanja.nedeljkovic@ikor.de>
2022-07-19 15:05:17 +0200

    Formatting

--- sip-core/src/main/java/de/ikor/sip/foundation/core/util/FoundationFeature.java
+++ sip-core/src/main/java/de/ikor/sip/foundation/core/util/FoundationFeature.java
@@ -1,14 +1,12 @@
 package de.ikor.sip.foundation.core.util;
 
-/**
- * Contains names of all SIP Foundation Features
- */
+/** Contains names of all SIP Foundation Features */
 public class FoundationFeature {
 
-  private FoundationFeature(){}
-  public static final String TRACING ="tracing";
+  private FoundationFeature() {}
+
+  public static final String TRACING = "tracing";
   public static final String INFO = "info";
   public static final String ADAPTER_ROUTES = "adapter-routes";
   public static final String HEALTH = "health";
-
 }
