28008717a337cbb88ef5f145c0624e55904f6a74
nikola.glidzic <nikola.glidzic@ikor.de>
2022-07-22 12:57:18 +0200

    Resolve one code smell

--- sip-test-kit/src/test/java/de/ikor/sip/foundation/testkit/workflow/whenphase/ExecutionWrapperTest.java
+++ sip-test-kit/src/test/java/de/ikor/sip/foundation/testkit/workflow/whenphase/ExecutionWrapperTest.java
@@ -30,7 +30,7 @@ class ExecutionWrapperTest {
 
     // assert
     assertThat(actual.getMessage().getHeader(RouteInvoker.TEST_NAME_HEADER)).isEqualTo(TEST_NAME);
-    assertThat(actual.getMessage().getHeader(ProcessorProxy.TEST_MODE_HEADER));
+    assertThat(actual.getMessage().getHeader(ProcessorProxy.TEST_MODE_HEADER, Boolean.class)).isTrue();
     assertThat(actual.getMessage().getBody()).isNull();
   }
 
