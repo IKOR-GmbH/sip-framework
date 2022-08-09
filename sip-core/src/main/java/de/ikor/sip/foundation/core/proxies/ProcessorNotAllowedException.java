8de4eab9ee45b46e72fed3a44611f21f7ded4931
Nemikor <nemanja.nedeljkovic@ikor.de>
2022-08-09 09:25:18 +0200

    Add parameter type of original processor to proxy

--- sip-core/src/main/java/de/ikor/sip/foundation/core/proxies/ProcessorNotAllowedException.java
+++
@@ -1,8 +0,0 @@
-package de.ikor.sip.foundation.core.proxies;
-
-public class ProcessorNotAllowedException extends RuntimeException {
-
-  public ProcessorNotAllowedException(String message) {
-    super(message);
-  }
-}
