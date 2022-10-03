0e2d56e041da8a44f3468300fbf3c6c42fe1fc2d
Leto Bukarica <leto.bukarica@gmail.com>
2022-10-03 17:49:23 +0200

    SIP-1315|added javadocs

--- sip-core/src/main/java/de/ikor/sip/foundation/core/util/CamelHelper.java
+++ sip-core/src/main/java/de/ikor/sip/foundation/core/util/CamelHelper.java
@@ -3,12 +3,19 @@ package de.ikor.sip.foundation.core.util;
 import org.apache.camel.Processor;
 import org.apache.camel.processor.WrapProcessor;
 
+/** Utility class to help with Camel's internals */
 public class CamelHelper {
 
   private CamelHelper() {
     throw new IllegalStateException("Utility class");
   }
 
+  /**
+   * If an instance of WrapProcessor is received it will return the unwrapped Processor
+   *
+   * @param wrappedProcessor - Processor that can be wrapped by Camel in a WrapProcessor class
+   * @return originalProcessor - Unwrapped processor
+   */
   public static Processor unwrapProcessor(Processor wrappedProcessor) {
     Processor originalProcessor = wrappedProcessor;
     while (originalProcessor instanceof WrapProcessor) {
