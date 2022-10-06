0488c1741d8ac25ff215ac38ecc5afe6be80d265
Nemikor <nemanja.nedeljkovic@ikor.de>
2022-10-06 12:13:25 +0200

    Change implementation by adding a property with sip default endpoints

--- sip-core/src/main/java/de/ikor/sip/foundation/core/actuator/config/EndpointExposureAutoConfiguration.java
+++
@@ -1,9 +0,0 @@
-package de.ikor.sip.foundation.core.actuator.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-/** Configuration class for including {@link EndpointExposureConfiguration} */
-@Configuration
-@ComponentScan
-public class EndpointExposureAutoConfiguration {}
--- sip-core/src/main/java/de/ikor/sip/foundation/core/actuator/config/EndpointExposureConfiguration.java
+++ sip-core/src/main/java/de/ikor/sip/foundation/core/actuator/config/EndpointExposureConfiguration.java
@@ -1,52 +1,67 @@
 package de.ikor.sip.foundation.core.actuator.config;
 
-import de.ikor.sip.foundation.core.annotation.SIPFeature;
+import java.io.IOException;
+import java.util.Objects;
 import java.util.Properties;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.PropertiesPropertySource;
-import org.springframework.util.ClassUtils;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 
 /** Collects all endpoint that should be exposed in actuator */
-@Configuration
-public class EndpointExposureConfiguration {
+public class EndpointExposureConfiguration
+    implements ApplicationListener<ApplicationEnvironmentPreparedEvent> {
 
   private static final String EXPOSURE_INCLUDE = "management.endpoints.web.exposure.include";
+  private static final String SIP_DEFAULTS = "sip.core.actuator.endpoints";
 
-  /**
-   * Collects all SIP actuator endpoints and adds them to list of endpoints defined in configuration
-   *
-   * @param environment {@link ConfigurableEnvironment}
-   * @param applicationContext {@link ApplicationContext}
-   * @return exposed endpoints
-   */
-  @Bean({"endpointExposure"})
-  public String generateExposedActuatorEndpoints(
-      ConfigurableEnvironment environment, ApplicationContext applicationContext) {
-    Properties props = new Properties();
-    String endpoints = resolveEndpoints(environment, applicationContext);
-    props.put(EXPOSURE_INCLUDE, endpoints);
-    PropertiesPropertySource testCasesPropertySource =
-        new PropertiesPropertySource("exposedEndpoints", props);
-    environment.getPropertySources().addFirst(testCasesPropertySource);
-    return endpoints;
+  @Override
+  public void onApplicationEvent(ApplicationEnvironmentPreparedEvent event) {
+    ConfigurableEnvironment environment = event.getEnvironment();
+    Properties exposedEndpoints = new Properties();
+    Properties loadedProperties = loadDefaultProperties();
+    String endpoints = resolveEndpoints(environment, loadedProperties);
+    exposedEndpoints.put(EXPOSURE_INCLUDE, endpoints);
+    PropertiesPropertySource endpointsPropertySource =
+        new PropertiesPropertySource("exposedEndpoints", exposedEndpoints);
+    environment.getPropertySources().addFirst(endpointsPropertySource);
   }
 
   private String resolveEndpoints(
-      ConfigurableEnvironment environment, ApplicationContext applicationContext) {
+      ConfigurableEnvironment environment, Properties loadedProperties) {
     String endpoints = environment.getProperty(EXPOSURE_INCLUDE);
     if (endpoints == null) {
-      return "";
+      endpoints = loadedProperties.getProperty(EXPOSURE_INCLUDE);
     }
-    if ("*".equals(endpoints)) {
-      return "*";
-    }
-    for (Object bean : applicationContext.getBeansWithAnnotation(SIPFeature.class).values()) {
-      SIPFeature sipFeature = ClassUtils.getUserClass(bean).getAnnotation(SIPFeature.class);
-      endpoints = endpoints.concat("," + sipFeature.name());
+    if (!"*".equals(endpoints)) {
+      String sipEndpoints = loadedProperties.getProperty(SIP_DEFAULTS);
+      endpoints = endpoints.concat("," + sipEndpoints);
     }
     return endpoints;
+  }
+
+  private Properties loadDefaultProperties() {
+    YamlPropertiesFactoryBean yamlPropertiesFactory = new YamlPropertiesFactoryBean();
+    yamlPropertiesFactory.setResources(getResources());
+    return Objects.requireNonNull(yamlPropertiesFactory.getObject());
+  }
+
+  private Resource[] getResources() {
+    ClassLoader classLoader = EndpointExposureConfiguration.class.getClassLoader();
+    ResourcePatternResolver resourcePatternResolver =
+        new PathMatchingResourcePatternResolver(classLoader);
+    try {
+      return resourcePatternResolver.getResources(getPath());
+    } catch (IOException e) {
+      throw new IllegalArgumentException("File not found for path " + getPath());
+    }
+  }
+
+  protected String getPath() {
+    return "sip-core-default-config.yaml";
   }
 }
