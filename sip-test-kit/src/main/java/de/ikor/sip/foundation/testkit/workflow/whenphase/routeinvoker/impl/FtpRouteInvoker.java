bd5e70e5df551b410000f789dc4d212b896216f3
nikola.glidzic <nikola.glidzic@ikor.de>
2022-10-13 10:39:46 +0200

    Delete unnecessary annotation

--- sip-test-kit/src/main/java/de/ikor/sip/foundation/testkit/workflow/whenphase/routeinvoker/impl/FtpRouteInvoker.java
+++ sip-test-kit/src/main/java/de/ikor/sip/foundation/testkit/workflow/whenphase/routeinvoker/impl/FtpRouteInvoker.java
@@ -12,7 +12,6 @@ import java.io.InputStream;
 import java.util.Map;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Endpoint;
 import org.apache.camel.Exchange;
@@ -27,7 +26,6 @@ import org.springframework.stereotype.Component;
 @Component
 @ConditionalOnClass(FtpComponent.class)
 @RequiredArgsConstructor
-@Slf4j
 public class FtpRouteInvoker implements RouteInvoker {
 
   private final CamelContext camelContext;
