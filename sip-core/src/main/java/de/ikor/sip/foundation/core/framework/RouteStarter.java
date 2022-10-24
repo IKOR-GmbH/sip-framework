d01d96af2a3d1897dc093860f29c971d9ff3dbde
nikola.glidzic <nikola.glidzic@ikor.de>
2022-10-24 11:28:33 +0200

    Adding framework auto config for automatic loading RouteStarter as bean

--- sip-core/src/main/java/de/ikor/sip/foundation/core/framework/RouteStarter.java
+++ sip-core/src/main/java/de/ikor/sip/foundation/core/framework/RouteStarter.java
@@ -1,17 +1,23 @@
 package de.ikor.sip.foundation.core.framework;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 import org.apache.camel.spi.CamelEvent;
 import org.apache.camel.support.EventNotifierSupport;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RouteStarter extends EventNotifierSupport {
-  @Autowired List<CentralRouter> availableRouters;
+  List<CentralRouter> availableRouters = new ArrayList<>();
+
+  public RouteStarter(Optional<List<CentralRouter>> availableRouters) {
+    availableRouters.ifPresent(centralRouters -> this.availableRouters = centralRouters);
+  }
 
   @Override
-  public void notify(CamelEvent event) throws Exception {
+  public void notify(CamelEvent event) {
     CentralRouter.setCamelContext(((CamelEvent.CamelContextInitializingEvent) event).getContext());
     availableRouters.forEach(this::buildRoutes);
   }
