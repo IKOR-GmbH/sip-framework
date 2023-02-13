9d8ae3a4008e9991b5454fa40048580c8ee67a72
nikola.glidzic <nikola.glidzic@ikor.de>
2023-02-13 10:58:15 +0100

    Add javadoc fro DeclarationRegistryApi. Change path in archetype for structure docs.

--- sip-core/src/main/java/de/ikor/sip/foundation/core/declarative/DeclarationRegistryApi.java
+++ sip-core/src/main/java/de/ikor/sip/foundation/core/declarative/DeclarationRegistryApi.java
@@ -8,33 +8,97 @@ import de.ikor.sip.foundation.core.declarative.scenario.IntegrationScenarioDefin
 import java.util.List;
 import java.util.Optional;
 
-// TODO: Add javadoc
+/**
+ * API interface for {@link DeclarationsRegistry} used within the framework structure.
+ */
 public interface DeclarationRegistryApi {
 
+  /**
+   * Get {@link ConnectorGroupDefinition} by its id.
+   *
+   * @param connectorGroupId is its id
+   * @return Optional connector group
+   */
   Optional<ConnectorGroupDefinition> getConnectorGroupById(final String connectorGroupId);
 
+  /**
+   * Get {@link IntegrationScenarioDefinition} by its id.
+   *
+   * @param scenarioId is its id
+   * @return Integration scenario
+   */
   IntegrationScenarioDefinition getScenarioById(final String scenarioId);
 
+  /**
+   * Get {@link ConnectorDefinition} by its id.
+   *
+   * @param connectorId is its id
+   * @return Optional connector
+   */
   Optional<ConnectorDefinition> getConnectorById(final String connectorId);
 
+  /**
+   * Get list of all {@link InboundConnectorDefinition}.
+   *
+   * @return all inbound connectors
+   */
   @SuppressWarnings("rawtypes")
   List<InboundConnectorDefinition> getInboundConnectors();
 
+  /**
+   * Get list of all {@link OutboundConnectorDefinition}.
+   *
+   * @return all outbound connectors
+   */
   List<OutboundConnectorDefinition> getOutboundConnectors();
 
+  /**
+   * Get list of {@link InboundConnectorDefinition} by id.
+   *
+   * @return inbound connectors
+   */
   @SuppressWarnings("rawtypes")
   Optional<InboundConnectorDefinition> getInboundConnectorById(String connectorId);
 
+  /**
+   * Get list of {@link OutboundConnectorDefinition} by id.
+   *
+   * @return outbound connectors
+   */
   Optional<OutboundConnectorDefinition> getOutboundConnectorById(String connectorId);
 
+  /**
+   * Get list of {@link InboundConnectorDefinition} by id of {@link ConnectorGroupDefinition}.
+   *
+   * @param connectorGroupId is its id
+   * @return inbound connectors
+   */
   @SuppressWarnings("rawtypes")
   List<InboundConnectorDefinition> getInboundConnectorsByConnectorGroupId(String connectorGroupId);
 
+  /**
+   * Get list of {@link OutboundConnectorDefinition} by id of {@link ConnectorGroupDefinition}.
+   *
+   * @param connectorGroupId is its id
+   * @return outbound connectors
+   */
   List<OutboundConnectorDefinition> getOutboundConnectorsByConnectorGroupId(
       String connectorGroupId);
 
+  /**
+   * Get list of {@link InboundConnectorDefinition} by id of {@link IntegrationScenarioDefinition}.
+   *
+   * @param scenarioId is its id
+   * @return inbound connectors
+   */
   @SuppressWarnings("rawtypes")
   List<InboundConnectorDefinition> getInboundConnectorsByScenarioId(String scenarioId);
 
+  /**
+   * Get list of {@link OutboundConnectorDefinition} by id of {@link IntegrationScenarioDefinition}.
+   *
+   * @param scenarioId is its id
+   * @return outbound connectors
+   */
   List<OutboundConnectorDefinition> getOutboundConnectorsByScenarioId(String scenarioId);
 }
