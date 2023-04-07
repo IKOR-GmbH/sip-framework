8cc7fe56e60a8039c0778d19b0cd7a84f60f482f
nikola.glidzic <nikola.glidzic@ikor.de>
2023-04-07 14:58:13 +0200

    Add unit test for covering specific processors Enricher and PollEnricher in RoutesRegistry

--- sip-core/src/test/java/de/ikor/sip/foundation/core/declarative/RoutesRegistryTest.java
+++ sip-core/src/test/java/de/ikor/sip/foundation/core/declarative/RoutesRegistryTest.java
@@ -10,7 +10,10 @@ import de.ikor.sip.foundation.core.proxies.ProcessorProxyRegistry;
 import de.ikor.sip.foundation.core.util.exception.SIPFrameworkInitializationException;
 import java.util.*;
 import org.apache.camel.*;
+import org.apache.camel.builder.ExpressionBuilder;
 import org.apache.camel.component.servlet.ServletConsumer;
+import org.apache.camel.processor.Enricher;
+import org.apache.camel.processor.PollEnricher;
 import org.apache.camel.processor.SendProcessor;
 import org.apache.camel.spi.CamelEvent;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,13 +22,10 @@ import org.junit.jupiter.api.Test;
 class RoutesRegistryTest {
 
   private static final String CONNECTOR_ID = "connectorId";
-
   private static final String GENERATED_ROUTE_ID = "sip-connector_connectorId_externalEndpoint";
-
+  private static final String BASE_HTTP_URI = "http://baseUri";
   private RoutesRegistry subject;
-
   private DeclarationsRegistry declarationsRegistry;
-
   private DeclarationsRegistryTest.InboundConnectorMock connector;
 
   @BeforeEach
@@ -105,12 +105,13 @@ class RoutesRegistryTest {
 
     Endpoint endpoint = mock(Endpoint.class);
     when(route.getEndpoint()).thenReturn(endpoint);
-    when(endpoint.getEndpointBaseUri()).thenReturn("http://baseUri");
+    when(endpoint.getEndpointBaseUri()).thenReturn(BASE_HTTP_URI);
 
+    // SendProcessor case
     SendProcessor sendProcessor = mock(SendProcessor.class);
     when(sendProcessor.getEndpoint()).thenReturn(endpoint);
 
-    List<Service> services = Arrays.asList(mock(ServletConsumer.class), sendProcessor);
+    List<Service> services = List.of(mock(ServletConsumer.class), sendProcessor);
     when(route.getServices()).thenReturn(services);
     routes.add(route);
     when(camelContext.getRoutes()).thenReturn(routes);
@@ -122,5 +123,52 @@ class RoutesRegistryTest {
 
     // assert
     assertThat(actualEndpointAware.get(0).getConnectorId()).isEqualTo(CONNECTOR_ID);
+  }
+
+  @Test
+  void
+      GIVEN_endpointAndSpecificProcessors_WHEN_generateRouteInfoList_THEN_expectValidRouteDeclarativeStructureInfo() {
+    // arrange
+    subject.generateRouteIdForConnector(RouteRole.EXTERNAL_ENDPOINT, connector);
+    CamelEvent.CamelContextEvent event = mock(CamelEvent.CamelContextEvent.class);
+    CamelContext camelContext = mock(ExtendedCamelContext.class);
+    ProcessorProxyRegistry proxyRegistry = mock(ProcessorProxyRegistry.class);
+    when(proxyRegistry.getProxies()).thenReturn(new HashMap<>());
+    when(event.getContext()).thenReturn(camelContext);
+    when(camelContext.getExtension(ProcessorProxyRegistry.class)).thenReturn(proxyRegistry);
+    List<Route> routes = new ArrayList<>();
+    Route route = mock(Route.class);
+    when(route.getRouteId()).thenReturn(GENERATED_ROUTE_ID);
+
+    Endpoint endpoint = mock(Endpoint.class);
+    when(route.getEndpoint()).thenReturn(endpoint);
+    when(endpoint.getEndpointBaseUri()).thenReturn(BASE_HTTP_URI);
+
+    // Enricher case
+    Endpoint enricherEndpoint = mock(Endpoint.class);
+    when(enricherEndpoint.getEndpointBaseUri()).thenReturn("uriForEnricher");
+    Enricher enricher = mock(Enricher.class);
+    when(enricher.getExpression()).thenReturn(ExpressionBuilder.simpleExpression("uriForEnricher"));
+
+    // PollEnricher case (without expression)
+    Endpoint pollEnricherEndpoint = mock(Endpoint.class);
+    when(pollEnricherEndpoint.getEndpointBaseUri()).thenReturn("pollEnrich-1");
+    PollEnricher pollEnricher = mock(PollEnricher.class);
+
+    List<Service> services = List.of(enricher, pollEnricher);
+    when(route.getServices()).thenReturn(services);
+    routes.add(route);
+    when(camelContext.getRoutes()).thenReturn(routes);
+
+    // act
+    subject.notify(event);
+    List<RouteDeclarativeStructureInfo> actualEnricher =
+        subject.generateRouteInfoList(pollEnricherEndpoint);
+    List<RouteDeclarativeStructureInfo> actualPollEnricher =
+        subject.generateRouteInfoList(pollEnricherEndpoint);
+
+    // assert
+    assertThat(actualEnricher.get(0).getConnectorId()).isEqualTo(CONNECTOR_ID);
+    assertThat(actualPollEnricher.get(0).getConnectorId()).isEqualTo(CONNECTOR_ID);
   }
 }
