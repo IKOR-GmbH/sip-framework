20eb88e866163e85682a9d556f1cc8074a74292a
Leto Bukarica <leto.bukarica@gmail.com>
2023-07-31 15:51:00 +0200

    SIp-1660|delete interface actually

--- sip-core/src/main/java/de/ikor/sip/foundation/core/declarative/orchestration/process/dsl/ProcessCallableWithinProviderDefinition.java
+++
@@ -1,7 +0,0 @@
-package de.ikor.sip.foundation.core.declarative.orchestration.process.dsl;
-
-/**
- * Marker interface for elements that can be nodes below a provider-definition within the scenario
- * orchestration DSL
- */
-public sealed interface ProcessCallableWithinProviderDefinition permits CallProcessConsumer {}
