dbdb24c74d335ad83847ac58c1e68672ee8c2a88
Nemanja Nedeljkovic <nemanja.nedeljkovic@ikor.de>
2023-08-08 11:48:37 +0200

    Use http client for rest test kit invoker  (#233)
    * Use rest template instead of producer template
    
    * Formatting
    
    * Add changelog
    
    * Add helper for common methods
    
    * Fix sonar issues

--- sip-test-kit/src/main/java/de/ikor/sip/foundation/testkit/workflow/whenphase/routeinvoker/impl/CxfRouteInvoker.java
+++ sip-test-kit/src/main/java/de/ikor/sip/foundation/testkit/workflow/whenphase/routeinvoker/impl/CxfRouteInvoker.java
@@ -1,27 +1,25 @@
 package de.ikor.sip.foundation.testkit.workflow.whenphase.routeinvoker.impl;
 
-import de.ikor.sip.foundation.core.proxies.ProcessorProxy;
+import static de.ikor.sip.foundation.testkit.util.HttpInvokerHelper.createExchangeResponse;
+import static de.ikor.sip.foundation.testkit.util.HttpInvokerHelper.prepareHeaders;
+
 import de.ikor.sip.foundation.testkit.util.TestKitHelper;
 import de.ikor.sip.foundation.testkit.workflow.whenphase.routeinvoker.RouteInvoker;
 import java.util.Optional;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Endpoint;
 import org.apache.camel.Exchange;
-import org.apache.camel.builder.ExchangeBuilder;
 import org.apache.camel.component.cxf.jaxws.CxfEndpoint;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.util.MultiValueMap;
 
 /** Invoker class for triggering Camel CXF(SOAP) route */
 @Component
@@ -54,38 +52,12 @@ public class CxfRouteInvoker implements RouteInvoker {
                 new ParameterizedTypeReference<>() {});
     log.trace("sip.testkit.workflow.whenphase.routeinvoker.soap.response_{}", response);
 
-    return Optional.of(createExchangeResponse(response));
+    return Optional.of(createExchangeResponse(response, camelContext));
   }
 
   @Override
   public boolean isApplicable(Endpoint endpoint) {
     return endpoint instanceof CxfEndpoint;
-  }
-
-  private Exchange createExchangeResponse(ResponseEntity<String> response) {
-    ExchangeBuilder exchangeBuilder =
-        ExchangeBuilder.anExchange(camelContext).withBody(formatToOneLine(response.getBody()));
-    response.getHeaders().forEach(exchangeBuilder::withHeader);
-    return exchangeBuilder.build();
-  }
-
-  private MultiValueMap<String, String> prepareHeaders(Exchange exchange) {
-    MultiValueMap<String, String> headers = new HttpHeaders();
-    headers.add(
-        RouteInvoker.TEST_NAME_HEADER,
-        exchange.getMessage().getHeader(RouteInvoker.TEST_NAME_HEADER, String.class));
-    headers.add(
-        ProcessorProxy.TEST_MODE_HEADER,
-        exchange.getMessage().getHeader(ProcessorProxy.TEST_MODE_HEADER, String.class));
-    return headers;
-  }
-
-  private String formatToOneLine(String multilineString) {
-    if (multilineString != null) {
-      return multilineString.lines().map(String::strip).collect(Collectors.joining(""));
-    }
-    log.trace("sip.testkit.workflow.whenphase.routeinvoker.soap.responseformating");
-    return null;
   }
 
   private String createAddressUri(Endpoint endpoint) {
--- sip-test-kit/src/test/java/de/ikor/sip/foundation/testkit/workflow/whenphase/routeinvoker/impl/RestRouteInvokerTest.java
+++ sip-test-kit/src/test/java/de/ikor/sip/foundation/testkit/workflow/whenphase/routeinvoker/impl/RestRouteInvokerTest.java
@@ -1,37 +1,68 @@
 package de.ikor.sip.foundation.testkit.workflow.whenphase.routeinvoker.impl;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
+import de.ikor.sip.foundation.core.proxies.ProcessorProxy;
 import de.ikor.sip.foundation.testkit.workflow.givenphase.Mock;
-import org.apache.camel.*;
+import de.ikor.sip.foundation.testkit.workflow.whenphase.routeinvoker.RouteInvoker;
+import java.util.Optional;
+import org.apache.camel.Exchange;
+import org.apache.camel.ExtendedCamelContext;
+import org.apache.camel.Route;
 import org.apache.camel.component.rest.RestEndpoint;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 class RestRouteInvokerTest {
 
   private static final String ROUTE_ID = "routeId";
+  public static final String TEST_RESPONSE = "test response";
 
   @Test
   void GIVEN_mockedExchangeAndEndpoint_WHEN_executeTask_THEN_verifySendingToGoodEndpointUri() {
     // arrange
     ExtendedCamelContext camelContext = mock(ExtendedCamelContext.class);
-    ProducerTemplate producerTemplate = mock(ProducerTemplate.class);
-    RestRouteInvoker subject = new RestRouteInvoker(producerTemplate, camelContext);
+    RestTemplateBuilder restTemplateBuilder = mock(RestTemplateBuilder.class);
+    RestTemplate restTemplate = mock(RestTemplate.class);
+    RestRouteInvoker subject =
+        new RestRouteInvoker(camelContext, mock(Environment.class), restTemplateBuilder);
     RestEndpoint restEndpoint = mock(RestEndpoint.class);
-    Exchange exchange = mock(Exchange.class);
+    Exchange exchange = mock(Exchange.class, RETURNS_DEEP_STUBS);
+    ResponseEntity<String> routeExpectedResponse =
+        new ResponseEntity<>(TEST_RESPONSE, HttpStatus.OK);
+    when(restTemplateBuilder.build()).thenReturn(restTemplate);
     when(restEndpoint.getMethod()).thenReturn("post");
     when(restEndpoint.getPath()).thenReturn("test");
-    when(producerTemplate.send("rest:post:test", exchange)).thenReturn(exchange);
+    when(restTemplate.exchange(
+            anyString(),
+            any(HttpMethod.class),
+            any(),
+            ArgumentMatchers.<ParameterizedTypeReference>any()))
+        .thenReturn(routeExpectedResponse);
 
     when(exchange.getProperty(Mock.ENDPOINT_ID_EXCHANGE_PROPERTY)).thenReturn(ROUTE_ID);
+    when(exchange.getMessage().getBody(String.class)).thenReturn("request");
+    when(exchange.getMessage().getHeader(RouteInvoker.TEST_NAME_HEADER, String.class))
+        .thenReturn("test");
+    when(exchange.getMessage().getHeader(ProcessorProxy.TEST_MODE_HEADER, String.class))
+        .thenReturn("true");
     Route route = mock(Route.class);
     when(camelContext.getRoute(ROUTE_ID)).thenReturn(route);
     when(route.getEndpoint()).thenReturn(restEndpoint);
 
     // act
-    subject.invoke(exchange);
+    Optional<Exchange> target = subject.invoke(exchange);
 
     // assert
-    verify(producerTemplate, times(1)).send("rest:post:test", exchange);
+    assertThat(target).isPresent();
+    assertThat(target.get().getMessage().getBody(String.class)).isEqualTo(TEST_RESPONSE);
   }
 }
