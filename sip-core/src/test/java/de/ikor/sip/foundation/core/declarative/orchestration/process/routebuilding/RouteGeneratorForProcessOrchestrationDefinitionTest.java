78c67d4c043ab3163bb527e7336bd1577743c047
Nemikor <nemanja.nedeljkovic@ikor.de>
2023-09-15 15:13:46 +0200

    RouteGeneratorForProcessOrchestrationDefinitionTest update

--- sip-core/src/test/java/de/ikor/sip/foundation/core/declarative/orchestration/process/routebuilding/RouteGeneratorForProcessOrchestrationDefinitionTest.java
+++ sip-core/src/test/java/de/ikor/sip/foundation/core/declarative/orchestration/process/routebuilding/RouteGeneratorForProcessOrchestrationDefinitionTest.java
@@ -1,14 +1,30 @@
 package de.ikor.sip.foundation.core.declarative.orchestration.process.routebuilding;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.*;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
+import de.ikor.sip.foundation.core.apps.declarative.ProcessOrchestrationConditionalAdapter;
 import de.ikor.sip.foundation.core.declarative.DeclarationsRegistry;
 import de.ikor.sip.foundation.core.declarative.DeclarationsRegistryApi;
+import de.ikor.sip.foundation.core.declarative.RoutesRegistry;
 import de.ikor.sip.foundation.core.declarative.orchestration.process.CompositeProcessOrchestrationInfo;
+import de.ikor.sip.foundation.core.declarative.orchestration.process.dsl.CallNestedCondition;
+import de.ikor.sip.foundation.core.declarative.orchestration.process.dsl.DSLTestHelper;
+import de.ikor.sip.foundation.core.declarative.orchestration.process.dsl.ProcessOrchestrationDefinition;
+import de.ikor.sip.foundation.core.declarative.orchestration.process.dsl.RouteGeneratorInternalHelper;
+import de.ikor.sip.foundation.core.declarative.scenario.IntegrationScenarioDefinition;
 import de.ikor.sip.foundation.core.util.exception.SIPFrameworkInitializationException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 import org.apache.camel.model.RoutesDefinition;
 import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
 
 class RouteGeneratorForProcessOrchestrationDefinitionTest {
 
@@ -37,5 +53,53 @@ class RouteGeneratorForProcessOrchestrationDefinitionTest {
         .isInstanceOf(SIPFrameworkInitializationException.class)
         .hasMessage(
             "Orchestration for composite process 'test' doesn't have a provider. Please define it.");
+  }
+
+  @Test
+  void when_NoBranchStatements_expect_LogWarning() {
+    // arrange
+    Logger logger =
+        (Logger) LoggerFactory.getLogger(RouteGeneratorForProcessOrchestrationDefinition.class);
+    ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
+    listAppender.start();
+    logger.setLevel(Level.DEBUG);
+    logger.addAppender(listAppender);
+    List<ILoggingEvent> logsList = listAppender.list;
+
+    CompositeProcessOrchestrationInfo compositeProcessOrchestrationInfo =
+        mock(CompositeProcessOrchestrationInfo.class, RETURNS_DEEP_STUBS);
+    CallNestedCondition callNestedCondition = DSLTestHelper.initCallNestedCondition();
+    RouteGeneratorInternalHelper.getConditionalStatements(callNestedCondition)
+        .add(new CallNestedCondition.ProcessBranchStatements(null, new ArrayList<>()));
+    when(compositeProcessOrchestrationInfo.getCompositeProcess().getId())
+        .thenReturn("ID of process");
+    RoutesDefinition routesDefinition = mock(RoutesDefinition.class, RETURNS_DEEP_STUBS);
+    when(compositeProcessOrchestrationInfo.getRoutesDefinition()).thenReturn(routesDefinition);
+    DeclarationsRegistry declarationsRegistry = mock(DeclarationsRegistry.class);
+    when(routesDefinition
+            .getCamelContext()
+            .getRegistry()
+            .findSingleByType(DeclarationsRegistryApi.class))
+        .thenReturn(declarationsRegistry);
+    when(routesDefinition.getCamelContext().getRegistry().findSingleByType(RoutesRegistry.class))
+        .thenReturn(mock(RoutesRegistry.class));
+    when(declarationsRegistry.getCompositeProcessProviderDefinition(any()))
+        .thenReturn(mock(IntegrationScenarioDefinition.class));
+    when(compositeProcessOrchestrationInfo.getConsumerEndpoints().keySet())
+        .thenReturn(Set.of(mock(ProcessOrchestrationConditionalAdapter.LoggingScenario.class)));
+    RouteGeneratorForProcessOrchestrationDefinition subject =
+        new RouteGeneratorForProcessOrchestrationDefinition(
+            compositeProcessOrchestrationInfo,
+            mock(ProcessOrchestrationDefinition.class, RETURNS_DEEP_STUBS));
+
+    // act
+    subject.generateRoutes(routesDefinition);
+    ILoggingEvent log = logsList.get(0);
+
+    // assert
+    assertThat(log.getMessage())
+        .contains(
+            "Orchestration for composite process '{}' does not call consumers '{}' for calls coming in from '{}'. Consider removing the consumer from the process definition.");
+    assertThat(log.getLevel()).isEqualTo(Level.WARN);
   }
 }
