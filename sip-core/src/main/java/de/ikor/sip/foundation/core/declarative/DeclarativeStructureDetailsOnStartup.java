3debec2c9759eaba6229570c86938a18916f1e4b
Leto Bukarica <leto.bukarica@gmail.com>
2024-01-05 11:13:24 +0100

    SIP-1597|changing start-up message

--- sip-core/src/main/java/de/ikor/sip/foundation/core/declarative/DeclarativeStructureDetailsOnStartup.java
+++ sip-core/src/main/java/de/ikor/sip/foundation/core/declarative/DeclarativeStructureDetailsOnStartup.java
@@ -1,5 +1,12 @@
 package de.ikor.sip.foundation.core.declarative;
 
+import de.ikor.sip.foundation.core.declarative.connector.ConnectorDefinition;
+import de.ikor.sip.foundation.core.declarative.connectorgroup.ConnectorGroupDefinition;
+import de.ikor.sip.foundation.core.declarative.process.CompositeProcessDefinition;
+import de.ikor.sip.foundation.core.declarative.scenario.IntegrationScenarioDefinition;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -15,12 +22,29 @@ public class DeclarativeStructureDetailsOnStartup {
 
   @EventListener(ApplicationReadyEvent.class)
   public void logDeclarativeStructureDetails() {
-    log.info(
-        "Successfully loaded: Connectors - {}, Integration Scenarios - {}, Processes - {}, Connector Groups - {} ",
-        declarationsRegistry.getConnectors().size(),
-        declarationsRegistry.getScenarios().size(),
-        declarationsRegistry.getProcesses().size(),
-        declarationsRegistry.getConnectorGroups().size());
+    log.info("SIP Framework Status:");
+    logElements("Connectors", declarationsRegistry.getConnectors(), ConnectorDefinition::getId);
+    logElements(
+        "Integration Scenarios",
+        declarationsRegistry.getScenarios(),
+        IntegrationScenarioDefinition::getId);
+    logElements(
+        "Composite Processes",
+        declarationsRegistry.getProcesses(),
+        CompositeProcessDefinition::getId);
+    logElements(
+        "Connector Groups",
+        declarationsRegistry.getConnectorGroups(),
+        ConnectorGroupDefinition::getId);
     log.info("SIP Framework - Adapter started and ready");
+  }
+
+  private static <T> void logElements(
+      String elementName, List<T> elements, Function<T, String> idExtractor) {
+    log.info(
+        "{} ({}): {}",
+        elementName,
+        elements.size(),
+        elements.stream().map(idExtractor).collect(Collectors.joining(", ")));
   }
 }
